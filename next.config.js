3d85d99b700672d8a4b8d2a3b948f9ca0e134e2a
vitae <anthonysuber@gmail.com>
2025-11-27 18:33:42 -1000

    Update next.config.js for Three.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['three'],
+  images: {
+    domains: [],
+  },
+  // Optimize for Three.js
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      bufferutil: 'commonjs bufferutil',
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
