23db29cba3fb7e53227a55969dc7a1664e1b3a59
vitae <anthonysuber@gmail.com>
2025-12-31 22:47:32 -1000

    embedded tix

--- app/mondays/page.tsx
+++ app/mondays/page.tsx
@@ -188,11 +188,7 @@ export default function MondaysPage() {
 
         {/* COMMUNITY / TICKETS */}
         <section id="community" className="flex flex-col items-center px-6 text-center space-y-4">
-          <div className="relative flex items-center justify-center rounded-full border border-white/30 backdrop-blur-md w-80 h-80 sm:w-96 sm:h-96 md:w-[28rem] md:h-[28rem] red-glow p-6">
-          
-            
             <CheckoutForm />
-          </div>
         </section>
 
       </main>
